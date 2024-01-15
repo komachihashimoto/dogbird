c87cd092c177471f4326ebae237ff471b1546a5f
komachihashimoto <joujinameko@gmail.com>
2024-01-15 16:57:59 +0900

    引数あり

--- dogbird.go
+++ dogbird.go
@@ -8,11 +8,6 @@ type dog struct {
 	height int
 }
 
-type bird struct {
-	name   string
-	group  string
-	length int
-}
 
 func (d dog) describe() string {
 	dscr := "わたしは" + d.group
@@ -20,16 +15,26 @@ func (d dog) describe() string {
 	return dscr
 }
 
-func (b bird) describe() string {
-	dscr := "わたしは" + b.name
-	dscr += "、" + b.group + "の仲間"
+func (d dog) bigger(d2 dog) string {
+
+	dscr := d.describe()
+	dscr += "、私は" + d2.name
+
+	diff := d.height - d2.height
+	switch {
+	case diff > 5:
+		dscr += "より大きい"
+	case diff < -5:
+		dscr += "より小さい"
+	default:
+		dscr += "と同じくらいである"
+	}
 	return dscr
 }
 
 func main() {
-	pome := dog{"ポメ","ポメラニアン", 25}
-	meji := bird{"メジロ","スズメ", 12}
+	maru := dog{"マル","マルチーズ", 22}
+	shiba := dog{"シバ","柴犬", 40}
 
-	fmt.Println(pome.describe())
-	fmt.Println(meji.describe())
+	fmt.Println(maru.bigger(shiba))
 }
